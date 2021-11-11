135071a4766433306b409bcc6e983407958c37be
Dario Pešerović <d.peserovic@gmail.com>
2021-11-11 18:26:55 +0100

    read and output json file

--- README.md
+++
@@ -1,7 +0,0 @@
-# ntpws-exercises
-
-## branch task-1: array exercise
-## branch task-2: form exercise
-## branch task-3: function exercise
-## branch task-4: if statment & rand() func exercise
-## branch task-5: calculator exercise
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+$cars = json_decode(file_get_contents('cars.json'), true);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <ol>
+        <?php
+        foreach ($cars as $car) {
+            echo '<li>' . $car['Name'] . ' - ' . $car['Year'] . ' - ' . $car['Origin'] . '</li>';
+        }
+        ?>
+    </ol>
+</body>
+
+</html>
